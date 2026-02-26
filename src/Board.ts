ab1bd3157b732835f2123def460c5da7da5436a4
Ariadna Perez <aperezgarrig@ukaachen.de>
2026-02-26 17:20:18 +0100

    Moved logic to constructor

--- src/Board.ts
+++ src/Board.ts
@@ -1,20 +1,25 @@
 export class Board {
   width;
   height;
+  private tiles: string[][];
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
+    this.tiles = [];
+
+    for (let y = 0; y < height; y++) {
+      this.tiles[y] = [];
+      for (let x = 0; x < width; x++) {
+        this.tiles[y][x] = ".";
+      }
+    }
   }
 
   toString(): string {
     let boardString:string = "";
-    let row:string = "";
-    for (let i:number = 0; i < this.width; i++) {
-      row += ".";
-    }
     for (let i:number = 0; i < this.height; i++) {
-      boardString += row + "\n";
+      boardString += this.tiles[i].join("") + "\n";
     }
     return boardString;
   }
